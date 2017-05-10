991dc53b0d98a0d28af018a9505f9601434da87c
Christian <christig@ucm.es>
2017-05-10 18:19:00 +0200

    changing the routing

--- server.py
+++ server.py
@@ -2,12 +2,13 @@
 
 from utils import *
 from flask import Flask, render_template, Response
-from flask import request
+from flask import request, url_for, redirect
 from random import randint
 from time import time
 from humbledb import Mongo, Document
 from sklearn.externals import joblib
 from sklearn.ensemble import RandomForestClassifier
+from urlparse import urlparse, urljoin
 import pickle
 
 class OPData(Document):
@@ -52,8 +53,6 @@ def form():
 		op["caps_lock"] = int(request.form["caps"])
 		op["time_by_press"] = float(request.form["time"]) / float(request.form["count"])
 
-		with Mongo:
-			OPData.insert(op)
 
 		vs = {}
 		features = [op["total_time"], op["phrase_len_words"], op["phrase_len_chars"], op["failed_exclamation_marks"], op["failed_dots"], op["failed_apostrophes"],
@@ -61,7 +60,7 @@ def form():
 					op["failed_words"]]
 		names = ["Alejandro", "Christian"]
 
-		clf = joblib.load('../clf.pkl')
+		clf = joblib.load('clf.pkl')
 		vs["realName"] = op["name"]
 		vs["predictName"] = names[int(clf.predict(features))]
 
@@ -87,6 +86,65 @@ def form():
 			values["apostrophes"] = OPPhrase.find()[rnd_num]["apostrophes"]
 
 		return render_template("form.html", vs=values)
+
+@app.route("/train", methods=["GET", "POST"])
+def train():
+	if request.method == "POST":
+		op = OPData()
+		op["original_phrase"] = request.form["phrase"]
+		op["user_phrase"] = request.form["usr_phrase"]
+
+		if len(op["original_phrase"].split()) != len(op["user_phrase"].split()):
+			values = {}
+			values["error"] = 1
+			values["timestamp"] = float(request.form["timestamp"])
+			values["phrase"] = op["original_phrase"]
+
+			return render_template("form.html", vs=values)
+
+		op["name"] = request.form["name"]
+		op["total_time"] = time() - float(request.form["timestamp"])
+		op["avg_time_keystroke"] = op["total_time"] / len(op["user_phrase"])
+		op["phrase_len_chars"] = len(op["original_phrase"])
+		op["phrase_len_words"] = len(op["original_phrase"].split())
+		op["failed_words"] = get_failed_words(op["original_phrase"], op["user_phrase"])
+		op["failed_chars"] = get_num_failed_chars(op["original_phrase"], op["user_phrase"])
+		op["failed_apostrophes"] = get_failed_punctuation_marks("'", request.form["apostrophes"], op["user_phrase"])
+		op["failed_dots"] = get_failed_punctuation_marks(".", request.form["dots"], op["user_phrase"])
+		op["failed_commas"] = get_failed_punctuation_marks(",", request.form["commas"], op["user_phrase"])
+		op["failed_question_marks"] = get_failed_punctuation_marks("?", request.form["question_marks"], op["user_phrase"])
+		op["failed_exclamation_marks"] = get_failed_punctuation_marks("!", request.form["exclamation_marks"], op["user_phrase"])
+		op["shift"] = int(request.form["shift"])
+		op["del"] = int(request.form["del"])
+		op["caps_lock"] = int(request.form["caps"])
+		op["time_by_press"] = float(request.form["time"]) / float(request.form["count"])
+
+		with Mongo:
+			OPData.insert(op)
+
+		return redirect(url_for("train"))
+
+	if request.method == "GET":
+		values = {}
+		values["error"] = 0
+		values["shift"] = 0
+		values["del"] = 0
+		values["caps"] = 0
+		values["time"] = 0
+		values["count"] = 0
+		values["timestamp"] = time()
+
+		with Mongo:
+			rnd_num = randint(0, OPPhrase.count() - 1)
+			values["phrase"] = OPPhrase.find()[rnd_num]["phrase"]
+			values["question_marks"] = OPPhrase.find()[rnd_num]["question_marks"]
+			values["exclamation_marks"] = OPPhrase.find()[rnd_num]["exclamation_marks"]
+			values["dots"] = OPPhrase.find()[rnd_num]["dots"]
+			values["commas"] = OPPhrase.find()[rnd_num]["commas"]
+			values["apostrophes"] = OPPhrase.find()[rnd_num]["apostrophes"]
+
+		return render_template("form2.html", vs=values)
+
 
 @app.route("/load", methods=["GET", "POST"])
 def loadPhrases():
